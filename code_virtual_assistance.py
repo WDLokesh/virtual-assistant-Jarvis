2e2c263ff32a75bf3f9a7cce0ff4cc7f8d44ccc4
JarvisLokesh <lmaharana061@gmai.com>
2022-07-29 01:15:08 +0530

    Uploaded in Git Public

--- code_virtual_assistance.py
+++ code_virtual_assistance.py
@@ -6,12 +6,9 @@ import datetime
 
 #From pyttsx3
 engine = pyttsx3.init('sapi5')
-voices= engine.getProperty('voices') #Shows details of current voice
-# print(voices[0].id)#TTS_MS_EN-US_DAVID_11.0
-# print(voices[1].id) #TTS_MS_EN-US_ZIRA_11.0
+voices= engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
 
-#speak function
 def speak(audio):
     engine.say(audio) 
     engine.runAndWait() #With this command, speech will not be audible to us.
